394adedb31dfefe2457bf46af62a7638c033090d
Rafiere <rafapgbr@gmail.com>
2023-04-21 14:54:09 -0300

    feat: add chapter 01 code

---
+++ chapter01-into-programming/chapter01.js
@@ -0,0 +1,73 @@
+/* Um statement em JS é uma operação como: */
+
+console.log("\nStatements\n")
+
+a = 2 * 2;
+
+console.log(a)
+
+/* Um statement é feito de uma ou mais expressões, como: */
+
+// b * 2;
+
+/* Obtendo o input com o prompt: */
+
+// age = prompt("Please, tell me your age: ")
+// console.log(age)
+
+/* Tipos de Valores */
+
+console.log("\nTipos de Valores\n")
+
+console.log("I'm a string!")
+
+console.log(42)
+
+console.log(true)
+
+/* Conversão entre tipos: */
+
+/* A função "Number()" é uma built-in function que realiza uma coerção explícita para o tipo "Number". */
+
+console.log("\nConversão entre Tipos\n")
+
+var c = "42";
+var d = Number(c)
+
+console.log(c)
+console.log(d)
+
+/* Comentários - Devem explicar o porquê, e não o quê. */
+
+console.log("\nComentários\n")
+
+// - Comentário de uma linha
+
+/*
+*   Comentário
+*   de
+*   múltiplas
+*   linhas.
+*/
+
+console.log("\nVariáveis\n")
+
+/* O JS é uma linguagem com tipagem dinâmica. As variáveis podem suportar valores de qualquer tipo sem nenhum reforço
+* de tipagem. */
+
+//Declarando uma variável e atribuindo um valor a ela:
+
+var amount = 99.99
+
+amount = amount * 2
+
+console.log(amount)
+
+// Constantes são valores que não serão alterados durante a execução do programa.
+const TAX_RATE = 0.08;
+
+console.log(TAX_RATE)
+
+console.log("\nBloco IF\n")
+console.log()
+//a
