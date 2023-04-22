79f9cb31275d88d9a70adbf302119c4d11263236
Rafiere <rafapgbr@gmail.com>
2023-04-22 14:28:21 -0300

    feat: add full chapter 02 code.

--- chapter02-into-javascript/chapter02.js
+++ chapter02-into-javascript/chapter02.js
@@ -129,3 +129,104 @@ function fora(){
 * dentro da função "fora()", ao executarmos a função "fora()", é retornada a função "somarXMais3()", e, ao executarmos
 * essa função, ela lembra do escopo em que ela estava, e lembra que o valor de "x" é "1005". */
 console.log(fora()());
+
+/* Modules */
+
+console.log("\nModules\n");
+
+/* O "module pattern" é uma forma de definirmos detalhes privados de implementações, como
+* variáveis ou funções, que estejam escondidos do mundo externo. */
+
+/* Nesse exemplo, todas as variáveis estão escondidas do mundo externo, e apenas
+* a "publicAPI", que está sendo retornada pela função "User", está visível. */
+
+function User(){
+    var username, password
+
+    function doLogin(user, pw) {
+        username = user;
+        password = pw;
+
+        console.log(user, pw)
+    }
+
+        var publicAPI = {
+            login: doLogin
+        };
+
+        return publicAPI
+}
+
+var fred = User();
+fred.login("Fred", "1234")
+
+/* Palavra-chave "this" */
+
+console.log("\nPalavra-chave 'this'\n");
+
+/* Se uma função tem um "this" dentro dela, essa referência normalmente aponta para um
+* objeto, mas o objeto em que ela aponta depende de como a função foi chamada. */
+
+function foo2(){
+    console.log(this.bar2)
+}
+
+var bar2 = "global";
+
+var obj1 = {
+    bar2: "obj2",
+    foo2: foo2
+}
+
+var obj2 = {
+    bar: "obj2"
+}
+
+foo2(); //"global"
+obj1.foo2() //"obj1"
+foo2.call(obj2) //Estamos chamando a função "foo2" no contexto do objeto "obj2", logo, o
+                //valor de "this.bar2" será o "obj2".
+new foo(); //estamos definindo o "this" para um novo objeto vazio.
+
+/* Prototypes */
+
+console.log("\nPrototypes\n");
+
+/* Quando uma referência para uma propriedade em um objeto não existir, o JS vai, automaticamente, usar
+* o protótipo interno do objeto para procurar por essa propriedade, é como se fosse um "fallback" caso a propriedade
+* esteja faltando. */
+
+/* Todos os objetos em JavaScript herdam de pelo menos um outro objeto. Um objeto comum herdará da classe
+* "Object", que é uma classe nativa que possui os métodos "valueOf", por exemplo. */
+
+/* Polyfill */
+
+console.log("\nPolyfill\n");
+
+/* Um polyfill é uma feature nova do JS que produz um pedaço de código que é equivalente ao
+* comportamento original, mas que pode ser executado em ambientes que rodam uma versão antiga do
+* JS. */
+
+/* Recurso novo (ES6) */
+
+Number.isNaN(1)
+
+/* Polyfill desse recurso: Se esse recurso não existe ainda, ele entrará no "if" e
+* criaremos essa função na mão, para que ela possa ser utilizada. */
+if(!Number.isNaN){
+    Number.isNaN = function isNaN(x){
+        return x !== x;
+    };
+}
+
+/* Transpilling */
+
+console.log("\nTranspilling\n");
+
+/* Quando uma nova sintaxe é inserida na linguagem, não há como efetuar o polyfill, porém, é
+* possível usarmos uma ferramenta que converte o código novo em códigos antigos que são equivalentes. */
+
+/* Isso é usado no TypeScript, por exemplo. Um transpilador muito utilizado é o Babel. */
+
+/* Funções como o "getElementById()" ou o "alert()" são providas pelo browser, e não pelo JS. O DOM, por
+* exemplo, é implementado usando C e C++. */
